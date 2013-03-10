0d27ed2636a0ed5f56f711a73c4483ecb08110e1
Maciej Chudak <macioosch@gmail.com>
2013-03-10 13:02:56 +0100

    fixed the phase plot

--- phaseplot.cpp
+++ phaseplot.cpp
@@ -1,34 +1,57 @@
 #include "phaseplot.h"
 #define MAXPOINTS 0x1000
 
-static QVector <double> x(MAXPOINTS), v(MAXPOINTS);
+static QVector <double> x, v;
 static int points = 0;
 
 void phasePlot(QCustomPlot *plt)
 {
+    static QCPCurve *vx;
+    static QCPItemText *textLabel;
+    static QCPItemLine *arrow;
+
     static bool plotadded = false;
     if (!plotadded)
     {
         plotadded = true;
-        plt->addGraph();
+        vx = new QCPCurve(plt->xAxis, plt->yAxis);
+        plt->addPlottable(vx);
         plt->xAxis->setLabel("x");
         plt->yAxis->setLabel("v");
         plt->xAxis->setRange(-2, 2);
         plt->yAxis->setRange(-2, 2);
+        plt->xAxis->setAutoTickStep(false);
+        plt->yAxis->setAutoTickStep(false);
+        plt->xAxis->setTickStep(0.5);
+        plt->yAxis->setTickStep(0.5);
+
+        // add the text label indicating t=0 point:
+        textLabel = new QCPItemText(plt);
+        plt->addItem(textLabel);
+        textLabel->setPositionAlignment(Qt::AlignTop|Qt::AlignHCenter);
+        textLabel->setText("t = 0");
+
+        // add the arrow:
+        arrow = new QCPItemLine(plt);
+        plt->addItem(arrow);
+        arrow->start->setParentAnchor(textLabel->bottom);
+        arrow->setHead(QCPLineEnding::esSpikeArrow);
     }
 
     if (points < MAXPOINTS)
     {
-        plt->graph(0)->clearData();
-        plt->graph(0)->setData( x, v);
+        vx->clearData();
+        vx->setData( x, v);
+        textLabel->position->setCoords(x[0]-0.3,v[0]+0.2);
+        arrow->end->setCoords(x[0],v[0]);
         plt->replot();
     }
 }
 
 void phasePlotReset()
 {
-    x.fill(0.0);
-    v.fill(0.0);
+    x.clear();
+    v.clear();
     points = 0;
 }
 
@@ -36,8 +59,8 @@ void phaseUpdateData(parameters p)
 {
     if (points < MAXPOINTS)
     {
-        x[points] = -p.x;
-        v[points] = p.v;
+        x += -p.x;
+        v +=  p.v;
         points++;
     }
 }
